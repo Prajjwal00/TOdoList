40d668d2625c07187d180989577dd0ed20c04469
Prajjwal00 <sharma.prajjwal453@gmail.com>
2022-04-05 13:58:40 +0530

    Initial commit

---
+++ precache-manifest.d58b4bfdf775c457385bb2a6ba934078.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d9df671d19b955f4875",
+    "url": "/static/css/main.4f046a75.chunk.css"
+  },
+  {
+    "revision": "2d9df671d19b955f4875",
+    "url": "/static/js/main.2d9df671.chunk.js"
+  },
+  {
+    "revision": "8f2f4211b0891106f089",
+    "url": "/static/js/1.8f2f4211.chunk.js"
+  },
+  {
+    "revision": "f2fdb6d53967368178ba",
+    "url": "/static/js/2.f2fdb6d5.chunk.js"
+  },
+  {
+    "revision": "769c94daf783692f2c65",
+    "url": "/static/js/runtime~main.769c94da.js"
+  },
+  {
+    "revision": "133e87a9006a1e39eb0be359d6e7c41e",
+    "url": "/index.html"
+  }
+];
